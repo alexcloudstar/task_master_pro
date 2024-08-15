72a92f4130f41ab8d9588eaf950e45f24878358f
alexcloudstar <alexcloudstar@gmail.com>
2024-08-16 02:09:28 +0300

    init: backend

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response , Application } from 'express';
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
