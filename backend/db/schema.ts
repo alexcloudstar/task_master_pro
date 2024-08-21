2afca7d1dc4bff1dd98e62d8c96bfd2f098fea8b
alexcloudstar <alexcloudstar@gmail.com>
2024-08-21 21:51:34 +0300

    feat: raw projects schema

--- backend/db/schema.ts
+++ backend/db/schema.ts
@@ -1,9 +1,41 @@
-import { pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { TTask } from '../src/lib/types';
+
+import { jsonb, pgEnum, pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+
+{/*
+- title: string - not null
+- description: string
+- created_at: timestamp - default now (start date)
+- updated_at: timestamp - default now
+- stories/tasks: Array<task>
+- color: string
+- status: enum(in progress, done, blocked, in review, etc) - Check Jira
+- assets (but stored in S3)
+- assigned to
+- created/reported_by
+- ?
+
+    */}
+
+export const EStatus = pgEnum('status', ['in_progress', 'done', 'blocked', 'in_review', 'closed']);
+
 
 export const project = pgTable('project', {
 	id: serial('id').primaryKey(),
-	name: varchar('name', { length: 255 }).notNull(),
-	description: varchar('description', { length: 255 }),
-	created_at: timestamp('created_at').defaultNow(),
-	updated_at: timestamp('updated_at').defaultNow(),
+    title: varchar('title').notNull(),
+    description: varchar('description'),
+    // Not sure if is the right way to store tasks we need an array
+    tasks: jsonb("tasks").$type<TTask[]>().array(),
+    color: varchar('color'),
+    status: EStatus('status').notNull(),
+    // TODO: Reference USER_ID
+    assigned_to: varchar('assigned_to'),
+    // TODO: Reference USER_ID
+    created_by: varchar('created_by'),
+    created_at: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+    updated_at: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
 });
