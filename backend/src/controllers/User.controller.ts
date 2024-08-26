b3b33925d2404cede7fce4d4ea7aa7f38fc54b20
alexcloudstar <alexcloudstar@gmail.com>
2024-08-26 23:28:05 +0300

    feat: Update Personal User Profile based on jwt token

--- backend/src/controllers/User.controller.ts
+++ backend/src/controllers/User.controller.ts
@@ -1,8 +1,9 @@
 import type { Request, Response } from 'express';
 import { clerk } from '../';
 import { db } from '../../db/drizzle';
-import { user } from '../../db/schema';
+import { TInsertUser, user } from '../../db/schema';
 import { eq } from 'drizzle-orm';
+import { jwtDecode } from 'jwt-decode';
 
 export type TUserRegister = {
 	first_name: string;
@@ -11,20 +12,25 @@ export type TUserRegister = {
 	password: string;
 };
 
-export type TUserLogin = {
-	id: string;
-} & Pick<TUserRegister, 'password'>;
+export type TUserUpdate = {
+	avatar: string;
+	cover: string;
+	username: string;
+	email: string;
+	first_name: string;
+	last_name: string;
+};
 
 const expiresInSeconds = 60 * 60 * 24 * 7;
 
 export const getUsers = async (_: Request, res: Response) => {
 	const users = await db.query.user.findMany();
 
-    if (!users.length) {
-        return res.status(404).json({
-            message: 'Users not found',
-        });
-    }
+	if (!users.length) {
+		return res.status(404).json({
+			message: 'Users not found',
+		});
+	}
 
 	return res.status(200).json({
 		users,
@@ -32,21 +38,78 @@ export const getUsers = async (_: Request, res: Response) => {
 };
 
 export const getUser = async (req: Request, res: Response) => {
-    const { id } = req.params;
+	const { id } = req.params;
 
 	const findedUser = await db.query.user.findFirst({
-        where: eq(user.id, +id),
-    });
+		where: eq(user.id, +id),
+	});
 
-    if (!findedUser) {
-        return res.status(404).json({
-            message: 'User not found',
-        });
-    }
+	if (!findedUser) {
+		return res.status(404).json({
+			message: 'User not found',
+		});
+	}
 
 	return res.status(200).json({
-        user: findedUser,
+		user: findedUser,
 	});
+};
+
+export const updateProfile = async (req: Request, res: Response) => {
+	try {
+		const {
+			avatar,
+			cover,
+			username,
+			email,
+			first_name,
+			last_name,
+		}: Omit<
+			TInsertUser,
+			'id' | 'clerk_id' | 'role' | 'created_at' | 'updated_at'
+		> = req.body;
+		const token = req.headers.authorization?.split(' ')[1];
+
+		if (!token) {
+			return res.status(401).json({
+				message: 'Unauthorized',
+			});
+		}
+
+		const decoded = jwtDecode(token);
+
+		const findedUser = await db.query.user.findFirst({
+			where: eq(user.clerk_id, decoded.sub ?? ''),
+		});
+
+		if (!findedUser) {
+			return res.status(404).json({
+				message: 'User not found',
+			});
+		}
+
+		const updated_user = await db
+			.update(user)
+			.set({
+				avatar,
+				cover,
+				username,
+				email,
+				first_name,
+				last_name,
+			})
+			.where(eq(user.clerk_id, decoded.sub ?? ''))
+			.returning();
+
+		return res.status(200).json({
+			user: updated_user,
+		});
+	} catch (error: any) {
+		console.log(error);
+		return res.status(500).json({
+			message: error?.errors?.[0]?.message,
+		});
+	}
 };
 
 // Only for testing purposes
@@ -81,5 +144,3 @@ export const register = async (req: Request, res: Response) => {
 		return res.status(500).json({ message: error?.errors?.[0]?.message });
 	}
 };
-
-
--- backend/src/routes/User.route.ts
+++ backend/src/routes/User.route.ts
@@ -1,10 +1,16 @@
-import { getUsers, getUser, register } from '../controllers/User.controller';
+import {
+	getUsers,
+	getUser,
+	register,
+	updateProfile,
+} from '../controllers/User.controller';
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/', getUsers);
 router.get('/:id', getUser);
+router.put('/', updateProfile);
 router.post('/signup', register);
 
 export default router;
