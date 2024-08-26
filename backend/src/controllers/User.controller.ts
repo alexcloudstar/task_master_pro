34107e7a659eb954a00096417ea454e011cec1b2
alexcloudstar <alexcloudstar@gmail.com>
2024-08-26 23:56:43 +0300

    feat: Get Profile/Delete Profile & other improvements to user controllers

--- backend/src/controllers/User.controller.ts
+++ backend/src/controllers/User.controller.ts
@@ -4,6 +4,7 @@ import { db } from '../../db/drizzle';
 import { TInsertUser, user } from '../../db/schema';
 import { eq } from 'drizzle-orm';
 import { jwtDecode } from 'jwt-decode';
+import { clerkClient } from '@clerk/clerk-sdk-node';
 
 export type TUserRegister = {
 	first_name: string;
@@ -49,11 +50,49 @@ export const getUser = async (req: Request, res: Response) => {
 			message: 'User not found',
 		});
 	}
+        
+    const { clerk_id, ...mappedUser } = findedUser;
 
 	return res.status(200).json({
-		user: findedUser,
+		user: mappedUser,
 	});
 };
+
+export const getProfile = async (req: Request, res: Response) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+
+        if (!token) {
+            return res.status(401).json({
+                message: 'Unauthorized',
+            });
+        }
+
+        const decoded = jwtDecode(token);
+
+        const findedUser = await db.query.user.findFirst({
+            where: eq(user.clerk_id, decoded.sub ?? ''),
+        });
+
+
+        if (!findedUser) {
+            return res.status(404).json({
+                message: 'User not found',
+            });
+        }
+
+        const { clerk_id, ...mappedUser } = findedUser;
+
+        return res.status(200).json({
+            user: mappedUser,
+        });
+    } catch (error: any) {
+        console.log(error);
+        return res.status(500).json({
+            message: error?.errors?.[0]?.message,
+        });
+    }
+}
 
 export const updateProfile = async (req: Request, res: Response) => {
 	try {
@@ -68,6 +107,7 @@ export const updateProfile = async (req: Request, res: Response) => {
 			TInsertUser,
 			'id' | 'clerk_id' | 'role' | 'created_at' | 'updated_at'
 		> = req.body;
+
 		const token = req.headers.authorization?.split(' ')[1];
 
 		if (!token) {
@@ -111,6 +151,44 @@ export const updateProfile = async (req: Request, res: Response) => {
 		});
 	}
 };
+
+export const deleteProfile = async (req: Request, res: Response) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+
+        if (!token) {
+            return res.status(401).json({
+                message: 'Unauthorized',
+            });
+        }
+
+        const decoded = jwtDecode(token);
+
+        const findedUser = await db.query.user.findFirst({
+            where: eq(user.clerk_id, decoded.sub ?? ''),
+        });
+
+        if (!findedUser) {
+            return res.status(404).json({
+                message: 'User not found',
+            });
+        }
+
+        await clerkClient.users.deleteUser(findedUser.clerk_id);
+
+        await db.delete(user).where(eq(user.clerk_id, decoded.sub ?? ''));
+
+        return res.status(204).json({
+            message: 'User deleted'
+        });
+    
+    } catch (error: any) {
+        console.log(error);
+        return res.status(500).json({
+            message: error?.errors?.[0]?.message,
+        });
+    }
+}
 
 // Only for testing purposes
 export const register = async (req: Request, res: Response) => {
--- backend/src/routes/User.route.ts
+++ backend/src/routes/User.route.ts
@@ -3,14 +3,18 @@ import {
 	getUser,
 	register,
 	updateProfile,
+    getProfile,
+    deleteProfile,
 } from '../controllers/User.controller';
 import { Router } from 'express';
 
 const router = Router();
 
 router.get('/', getUsers);
+router.get('/me', getProfile);
 router.get('/:id', getUser);
 router.put('/', updateProfile);
+router.delete('/', deleteProfile);
 router.post('/signup', register);
 
 export default router;
