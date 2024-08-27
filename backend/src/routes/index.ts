e38dfdc5e488ee6ee6b8076553bd2aca14449922
alexcloudstar <alexcloudstar@gmail.com>
2024-08-27 21:37:51 +0300

    mv: /api/user/signup -> /signup

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Application } from 'express';
 import user from './User.route';
 import project from './Project.route';
+import { register } from '../controllers/User.controller';
 
 export default (app: Application): void => {
+    // only used for testing
+    app.post('/signup', register);
 	app.use('/api/user', user);
 	app.use('/api/project', project);
 };
