66e640da52d86e822f3d6dc6cbb51339d48032ba
alexcloudstar <alexcloudstar@gmail.com>
2024-08-27 21:39:21 +0300

    fmt

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,8 +4,8 @@ import project from './Project.route';
 import { register } from '../controllers/User.controller';
 
 export default (app: Application): void => {
-    // only used for testing
-    app.post('/signup', register);
+	// only used for testing
+	app.post('/signup', register);
 	app.use('/api/user', user);
 	app.use('/api/project', project);
 };
