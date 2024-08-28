9f7007730a4349e6591b78f0ddcd9dd4287dd808
alexcloudstar <alexcloudstar@gmail.com>
2024-08-28 11:14:18 +0300

    feat: project test file

---
+++ backend/src/test/project.test.ts
@@ -0,0 +1,59 @@
+import { constants, env } from 'config';
+import { TSelectProject } from 'db/schema';
+import { describe, expect, test, beforeAll } from 'vitest';
+
+describe('/api/user', () => {
+	let response: Response;
+	let body: { projects: TSelectProject[] };
+
+	beforeAll(async () => {
+		response = await fetch('http://localhost:8000/api/project', {
+			headers: {
+				Authorization: `Bearer ${env.TEST_TOKEN}`,
+			},
+		});
+		body = await response.json();
+	}, constants.BEFORE_ALL_TIMEOUT);
+
+	test('Should have response status 200', () => {
+		expect(response.status).toBe(200);
+	});
+
+	test('Should return an array of users', () => {
+		expect(Array.isArray(body.projects)).toBe(true);
+	});
+
+	test('Should return an array of projects', () => {
+		expect(body.projects.length).toBeGreaterThan(0);
+	});
+
+	test('Should return an array of projects object', () => {
+		const keysOfProject = Object.keys(body.projects[0]);
+
+		expect(keysOfProject).toMatchObject(keysOfProject);
+	});
+});
+
+describe('/api/project/:id', () => {
+	let response: Response;
+	let body: { project: TSelectProject };
+
+	beforeAll(async () => {
+		response = await fetch('http://localhost:8000/api/project/1', {
+			headers: {
+				Authorization: `Bearer ${env.TEST_TOKEN}`,
+			},
+		});
+		body = await response.json();
+	}, constants.BEFORE_ALL_TIMEOUT);
+
+	test('Should have response status 200', () => {
+		expect(response.status).toBe(200);
+	});
+
+	test('Should return an project typeof TSelectProject', () => {
+		const keysOfProject = Object.keys(body.project);
+
+		expect(keysOfProject).toMatchObject(keysOfProject);
+	});
+});
