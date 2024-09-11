465f715d4285528dc1b72aac3b1638631f8ca0a7
alexcloudstar <alexcloudstar@gmail.com>
2024-09-11 17:39:32 +0300

    feat: shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
+import { Button } from "./components/ui/button"
 
 const App = () => {
-    return <div>Hello, World!</div>
+    return <div>
+        <h1>Hello World!</h1>
+        <Button>Click me</Button>
+    </div>
 }
 
 export default App
