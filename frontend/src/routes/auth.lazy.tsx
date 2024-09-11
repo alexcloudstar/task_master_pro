97a2260458770da5619941996f25509349d5983a
alexcloudstar <alexcloudstar@gmail.com>
2024-09-11 22:21:38 +0300

    feat: Tanstack Query

--- frontend/src/routes/auth.lazy.tsx
+++ frontend/src/routes/auth.lazy.tsx
@@ -1,9 +1,40 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
 
 const Auth = () => {
+  const formSchema = z.object({
+    first_name: z.string().min(2).max(255),
+    last_name: z.string().min(2).max(255),
+    email: z.string().email(),
+    password: z.string().min(8).max(255),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <div className='w-full lg:grid lg:min-h-[600px] xl:min-h-[800px] h-[calc(100%-24px)]'>
       <div className='flex items-center justify-center py-12'>
@@ -14,35 +45,64 @@ const Auth = () => {
               Enter your email below to login to your account
             </p>
           </div>
-          <div className='grid gap-4'>
-            <div className='grid gap-2'>
-              <Label htmlFor='email'>Email</Label>
-              <Input
-                id='email'
-                type='email'
-                placeholder='m@example.com'
-                required
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className='grid gap-4'>
+              <FormField
+                control={form.control}
+                name='first_name'
+                render={({ field }) => (
+                  <FormItem className='grid gap-2'>
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder='John' {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
               />
-            </div>
-            <div className='grid gap-2'>
-              <div className='flex items-center'>
-                <Label htmlFor='password'>Password</Label>
-                <a
-                  href='/forgot-password'
-                  className='ml-auto inline-block text-sm underline'
-                >
-                  Forgot your password?
-                </a>
-              </div>
-              <Input id='password' type='password' required />
-            </div>
-            <Button type='submit' className='w-full'>
-              Login
-            </Button>
-            <Button variant='outline' className='w-full'>
-              Login with Google
-            </Button>
-          </div>
+              <FormField
+                control={form.control}
+                name='last_name'
+                render={({ field }) => (
+                  <FormItem className='grid gap-2'>
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder='Doe' {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='email'
+                render={({ field }) => (
+                  <FormItem className='grid gap-2'>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder='john@doe.com' {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='password'
+                render={({ field }) => (
+                  <FormItem className='grid gap-2'>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type='password' {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button type='submit' className='w-full'>
+                Login
+              </Button>
+              <Button variant='outline' className='w-full'>
+                Login with Google
+              </Button>
+            </form>
+          </Form>
           <div className='mt-4 text-center text-sm'>
             Don&apos;t have an account?{' '}
             <a href='#' className='underline'>
