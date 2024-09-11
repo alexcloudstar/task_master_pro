fca9ec6275ff32698c9cdebee8bb8741fde7729d
alexcloudstar <alexcloudstar@gmail.com>
2024-09-11 18:03:07 +0300

    fmt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import { Button } from "./components/ui/button"
+import { Button } from './components/ui/button';
 
 const App = () => {
-    return <div>
-        <h1>Hello World!</h1>
-        <Button>Click me</Button>
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Button>Click me</Button>
     </div>
-}
+  );
+};
 
-export default App
+export default App;
