f85095e0ac55052719f85c9db0efc2250e43c896
alexcloudstar <alexcloudstar@gmail.com>
2024-09-12 23:21:24 +0300

    fmt

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,66 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Input } from './ui/input';
+import { SignedIn, SignOutButton, useUser } from '@clerk/clerk-react';
+import BreadcrumbNav from './BreadcrumbNav';
+import { Button } from './ui/button';
+import { Link, Search } from 'lucide-react';
+
+const Header = () => {
+  const { user } = useUser();
+
+  return (
+    <header className='flex items-center justify-between'>
+      <BreadcrumbNav />
+      <div className='flex gap-5'>
+        <div className='relative ml-auto flex-1 md:grow-0'>
+          <Search className='absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground' />
+          <Input
+            type='search'
+            placeholder='Search...'
+            className='w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]'
+          />
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant='outline'
+              size='icon'
+              className='overflow-hidden rounded-full'
+            >
+              <img
+                src={user?.imageUrl}
+                width={36}
+                height={36}
+                alt='Avatar'
+                className='overflow-hidden rounded-full'
+              />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link to='/settings'>Settings</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>Support</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <SignedIn>
+                <SignOutButton>Sign Out</SignOutButton>
+              </SignedIn>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import AsideNavigation from '@/components/AsideNavigation';
+import Header from '@/components/Header';
 import { TanStackRouterDevtools } from '@/components/TanStackRouterDevtools';
 import { useAuth } from '@clerk/clerk-react';
 import { createRootRoute, Outlet } from '@tanstack/react-router';
@@ -10,7 +11,12 @@ const Main = () => {
   return (
     <>
       {auth.isSignedIn && <AsideNavigation />}
-      <Outlet />
+      <div className='flex min-h-screen w-full flex-col'>
+        <main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8'>
+          {auth.isSignedIn && <Header />}
+          <Outlet />
+        </main>
+      </div>
       <Suspense>
         <TanStackRouterDevtools position='bottom-right' />
       </Suspense>
