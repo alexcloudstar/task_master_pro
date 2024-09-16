29d6b83de686387c976f7f46e7f5a62920a6b7f0
alexcloudstar <alexcloudstar@gmail.com>
2024-09-16 23:31:20 +0300

    fmt

--- frontend/src/components/pages/Projects/Add.tsx
+++ frontend/src/components/pages/Projects/Add.tsx
@@ -10,7 +10,15 @@ import {
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Plus } from 'lucide-react';
@@ -19,19 +27,19 @@ import { z } from 'zod';
 
 const Add = () => {
   const formSchema = z.object({
-        title: z.string().min(2).max(255),
+    title: z.string().min(2).max(255),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-        title: '',
+      title: '',
     },
   });
 
-    const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log(values);
-    }
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
 
   return (
     <div className='flex items-center justify-between'>
@@ -49,22 +57,24 @@ const Add = () => {
             <AlertDialogHeader>
               <AlertDialogTitle>Add a new Project</AlertDialogTitle>
               <AlertDialogDescription>
-<Form {...form}>
-<FormField
-  control={form.control}
-  name="title"
-  render={({ field }) => (
-    <FormItem>
-      <FormLabel>Project Title</FormLabel>
-      <FormControl>
-        <Input placeholder="shadcn" {...field} />
-      </FormControl>
-      <FormDescription>This is your public display name.</FormDescription>
-      <FormMessage />
-    </FormItem>
-  )}
-/>
-</Form>
+                <Form {...form}>
+                  <FormField
+                    control={form.control}
+                    name='title'
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Project Title</FormLabel>
+                        <FormControl>
+                          <Input placeholder='shadcn' {...field} />
+                        </FormControl>
+                        <FormDescription>
+                          This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </Form>
               </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
