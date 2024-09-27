dc25eb7d38996be433fcfd649dd775da83c95d8d
alexcloudstar <alexcloudstar@gmail.com>
2024-09-27 19:11:13 +0300

    fmt

--- frontend/src/services/projects/types.ts
+++ frontend/src/services/projects/types.ts
@@ -10,5 +10,5 @@ export type TProject = {
 
 export type TInsertProject = {
   id?: TProject['id'];
-created_by_id?: TProject['created_by_id'];
+  created_by_id?: TProject['created_by_id'];
 } & Omit<TProject, 'id' | 'created_by_id'>;
