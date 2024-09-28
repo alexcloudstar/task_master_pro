a87d71647aabd613ecb312e8c517d9f464832f7e
alexcloudstar <alexcloudstar@gmail.com>
2024-09-28 16:14:14 +0300

    feat: File Upload

--- backend/src/controllers/Files.controller.ts
+++ backend/src/controllers/Files.controller.ts
@@ -1,7 +1,9 @@
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { env } from '../../config';
 import { Request, Response } from 'express';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { Upload } from '@aws-sdk/lib-storage';
+import multer from 'multer';
 
 const client = new S3Client({
     region: env.AWS_REGION ?? "",
@@ -10,6 +12,7 @@ const client = new S3Client({
         secretAccessKey: env.AWS_SECRET_ACCESS_KEY ?? "",
     },
 });
+
 
 export const getFile = async (req: Request, res: Response) => {
 	const { folder, file } = req.params;
@@ -34,6 +37,44 @@ export const getFile = async (req: Request, res: Response) => {
             url: signedUrl,
         });
 
+	} catch (error) {
+		return res.status(500).json({
+			message: 'Internal Server Error',
+		});
+	}
+};
+
+
+export const uploadFile = async (req: Request, res: Response) => {
+    const { folder } = req.params;
+
+    if(!req.file) {
+        return res.status(400).json({
+            message: 'File is required',
+        });
+    }
+
+    const params = {
+        Bucket: env.AWS_BUCKET,
+        Key: `${folder}/${req.file.originalname}-${Date.now()}`,
+        Body: req.file.buffer,
+        ContentType: req.file.mimetype,
+    }
+
+	try {
+
+
+        const command = new PutObjectCommand(params);
+        await client.send(command);
+
+        const signedUrl = await getSignedUrl(client, new GetObjectCommand({
+            Bucket: env.AWS_BUCKET,
+            Key: params.Key,
+        }));
+
+        return res.status(200).json({
+            url: signedUrl,
+        });
 	} catch (error) {
 		return res.status(500).json({
 			message: 'Internal Server Error',
--- backend/src/routes/Files.route.ts
+++ backend/src/routes/Files.route.ts
@@ -1,10 +1,14 @@
+import multer from 'multer';
 import { getFile, uploadFile } from '../controllers/Files.controller';
 import { Router } from 'express';
 
 const router = Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
 router.get('/:folder/:file', getFile);
-router.post('/', uploadFile);
+router.post('/:folder', upload.single('file'), uploadFile);
 
 
 export default router;
